c01f3c1c23872cff380f111af3a3f97cacb55d6a
Александр Землянский <donaldasus86@gmail.com>
2026-09-22 15:48:07 +0300

    Update InputReader.cs

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -21,22 +21,4 @@ public class InputReader : MonoBehaviour
             DiractionInputing?.Invoke(Input.GetAxis(Horizontal), Input.GetAxis(Vertical));
         }
     }
-
-    public bool MouseLeftButtonClicked
-    {
-        get
-        {
-            return Input.GetMouseButtonDown(0); 
-        }
-    }
-
-    public float InputHorizontalDiraction()
-    {
-        return Input.GetAxis(Horizontal);
-    }
-
-    public float InputVerticalDiraction()
-    {
-        return Input.GetAxis(Vertical);
-    }
 }
